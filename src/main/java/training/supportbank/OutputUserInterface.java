b206ea62f78eea988ae514220a5f84079451d19f
Mike <Walker>
2019-10-25 00:40:52 +0100

    Add output user interface.

--- src/main/java/training/supportbank/OutputUserInterface.java
+++ src/main/java/training/supportbank/OutputUserInterface.java
@@ -1,6 +1,7 @@
 package training.supportbank;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class OutputUserInterface {
     private final Bank bank;
@@ -10,13 +11,28 @@ public class OutputUserInterface {
     }
 
     public void run() {
-        printAccounts(bank.getAccounts());
+        Scanner scanner = new Scanner(System.in);
+
+        boolean shouldContinue = true;
+        while (shouldContinue) {
+            System.out.println("Which function would you like to run?");
+            String selectedMethod = scanner.nextLine();
+
+            if (selectedMethod.equals("List All")) {
+                printAccounts();
+            }
+            else {
+                System.out.println("Sorry - it doesn't look like that is a valid function.");
+            }
+            System.out.println("Would you like to continue?");
+            shouldContinue = List.of("y", "yes").contains(scanner.nextLine().toLowerCase());
+        }
     }
 
-    private void printAccounts(List<Account> accounts) {
+    private void printAccounts() {
         System.out.printf("%-20s %10s %10s %10s%n", "Name", "In", "Out", "Total");
         System.out.println("-----------------------------------------------------");
-        for (Account account : accounts) {
+        for (Account account : bank.getAccounts()) {
             System.out.printf("%-20s %10.2f %10.2f %10.2f%n",
                     account.getName(),
                     account.getTotalIncomingValue(),
